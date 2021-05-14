e7d3a58ededebd1c4e4b475cb234be013aa75b2e
Klaus Wiese <klauswiese@users.noreply.github.com>
2021-05-14 11:13:27 -0600

    Update ANIRsentinel.R

--- R/ANIRsentinel.R
+++ R/ANIRsentinel.R
@@ -6,6 +6,7 @@ ANIRsentinel <- function(Red, NIR, SWIR){
     c <- (sqrt((1.6137-0.664)^2+(z-x)^2))
     d <- (a^2+b^2-c^2)/(2*a*b)
     d2 <- ifelse(d > 1, 1, ifelse(d < -1, -1, d))
+    #arco coseno
     ind <- acos(d2)
     return(ind)
   })
