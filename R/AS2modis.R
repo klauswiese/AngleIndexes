de2d832ed5b353b67fb9645f85cb4b1cefa7207f
Hondunauta <klauswiese@users.noreply.github.com>
2021-09-15 16:46:02 -0600

    Update AS2modis.R

--- R/AS2modis.R
+++ R/AS2modis.R
@@ -5,7 +5,7 @@ AS2modis <- function(SWIR, SWIR2, SWIR3){
     b <- (sqrt((2.130-1.640)^2+(y-z)^2))
     c <- (sqrt((2.130-1.240)^2+(z-x)^2))
     d <- (a^2+b^2-c^2)/(2*a*b)
-    d2 <- ifelse(d > 1, 1, ifelse(d < -1, -1, d))
+    d2 <- ifelse(d > 1, 1, ifelse(d < -1, -1, d))#revisar
     ind <- acos(d2)
     return(ind)
   })
